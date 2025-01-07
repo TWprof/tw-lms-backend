e579ef175a828a542cce0055c928c434a23e8f9f
Maeyy23 <msmabel23@gmail.com>
2025-01-07 10:28:42 +0100

    route error

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -6,7 +6,7 @@ const router = Router();
 router.post("/register", adminController.createAdmin);
 router.post("/set-password", adminController.setPassword);
 router.post("/login", adminController.adminLogin);
-router.get("/tutor/:tutorId", adminController.getTutor);
 router.get("/tutor/all", adminController.getAllTutors);
 
+router.get("/tutor/:tutorId", adminController.getTutor);
 export default router;
