b2bc6627f7d4bd22b2d6fde356deff1d918bc1eb
Maeyy23 <msmabel23@gmail.com>
2025-10-03 08:28:49 +0100

    Email debug

--- src/classes/studentClass.js
+++ src/classes/studentClass.js
@@ -48,6 +48,7 @@ export default class StudentClass {
       message: emailTemplate,
     };
     // send email by calling sendMail function
+
     await sendMail(emailPayload, constants.verifyEmail);
 
     const data = {
--- src/utils/mail.js
+++ src/utils/mail.js
@@ -6,10 +6,13 @@ const sendEmail = async (option, type) => {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: parseInt(process.env.EMAIL_PORT, 10),
-      secure: false,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
+      },
+      tls: {
+        rejectUnauthorized: false,
       },
     });
 
@@ -26,7 +29,7 @@ const sendEmail = async (option, type) => {
     }
 
     const emailOptions = {
-      from: "TECHWARE SERVICES <info@techware.ng>",
+      from: `"Techware Academy" <${process.env.EMAIL_USER}>`,
       to: option.to,
       subject,
       html: option.message,
@@ -40,8 +43,8 @@ const sendEmail = async (option, type) => {
     console.log("Message sent: %s", info.messageId);
     return info;
   } catch (error) {
-    console.error("Failed to send email: ", error);
-    throw error;
+    console.error("Failed to send email: ", error.message);
+    console.error(error);
   }
 };
 
